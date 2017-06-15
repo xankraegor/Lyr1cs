0378162212096ebd082ef2c41e0484e832e69646
Xan Kraegor <xankraegor@xankraegor.com>
2017-06-16 01:24:19 +0300

    Code cleaning

--- Lyr1cs/ViewController.swift
+++ Lyr1cs/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import MediaPlayer
-import AVFoundation
 
 class ViewController: UIViewController {
+
+    // MARK: - Outlets
 
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var songTitle: UILabel!
@@ -19,6 +20,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var noLyricsLabel: UILabel!
 
+    // MARK: - Variables
+
     var musicPlayer: MPMusicPlayerController?
     var nowPlayingSong: MPMediaItem? {
         didSet {
@@ -26,16 +29,14 @@ class ViewController: UIViewController {
         }
     }
 
+    // MARK: - View life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        lyricsTextView.layer.cornerRadius = 10.0
         labelsBackgroundView.layer.cornerRadius = 10.0
-        lyricsTextView.clipsToBounds = true
-
         musicPlayer = MPMusicPlayerController.systemMusicPlayer()
         NotificationCenter.default.addObserver(self, selector: #selector(self.playingItemDidChange), name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange, object: musicPlayer)
         musicPlayer!.beginGeneratingPlaybackNotifications()
-         imageView.backgroundColor = UIColor(colorLiteralRed: 245/255, green: 245/255, blue: 245/255, alpha: 1)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -44,29 +45,24 @@ class ViewController: UIViewController {
         }
     }
 
+    // MARK: - Notifications
+
     func playingItemDidChange(notification: NSNotification) {
         nowPlayingSong = musicPlayer?.nowPlayingItem
     }
 
+    // MARK: - Helper functions
+
     func getMetadata() {
         if let songUrl = nowPlayingSong?.value(forProperty: MPMediaItemPropertyAssetURL) as? URL {
-
             let playerItem = AVURLAsset(url: songUrl, options: nil)
             lyricsTextView.text = playerItem.lyrics
-            if playerItem.lyrics != nil {
-                noLyricsLabel.isHidden = true
-            } else {
-                noLyricsLabel.isHidden = false
-            }
-
+            noLyricsLabel.isHidden = ( playerItem.lyrics != nil ? true : false )
             lyricsTextView.setContentOffset(CGPoint.zero, animated: true)
-
             artistLabel.text = nowPlayingSong?.artist ?? "—"
             songTitle.text = nowPlayingSong?.title ?? "—"
-
             let imageSize = CGSize(width: view.bounds.width, height: view.bounds.height)
             imageView.image = nowPlayingSong?.artwork?.image(at: imageSize)
-
         } else {
             lyricsTextView.text = ""
             songTitle.text = "—"
